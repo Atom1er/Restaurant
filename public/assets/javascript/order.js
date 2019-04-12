47d3d0991ee7a4e326be85d19c182d63a2803a9d
Thomas <at93om@gmail.com>
2019-04-12 03:10:47 -0400

    SMS alert Done

--- public/assets/javascript/order.js
+++ public/assets/javascript/order.js
@@ -1,44 +1,137 @@
-$(document).ready(function(){
-    $.ajax({
-        method: 'GET',
-        url: '/api/menu'
-    }).then( function(data, status){
-        //HTML checkboxe tag Beginning
-        var checkboxe = '<div class="form-check">';
-        checkboxe += '<input class="form-check-input" type="checkbox" value="" id="';
-        //Middle
-        var checkboxeMiddle = '"><label class="form-check-label" for="defaultCheck1">'
-        //End
-        var checkboxEnd = '</label></div>';
-        //full tag = checkboxe + "id" + checkboxeMiddle + PlaceHolder + checkboxeEnd
-        // console.log(data);
-        console.log(data);
-        var menu_div= $('<div  id="menu-div">');
-        var ul = $('<ul>  id="menu-div-list"');
-        var li = $("<li>");
-        for(var i = 0; i < data.length; i++){
-            const element = data[i][0];
-            var h3 = $("<h3>");
-            h3.append(element);
-            
-            li.append(h3);
+$(document).ready(function () {
+    var order = [];
+    var plat_dibi = ["Dibi"];
+    var plat_riz = ["Riz"];
+    var plat = [];
 
-            var plat = data[i];
-            var plat_ul = $("<ul>");
-            for (let j = 1; j < plat.length; j++) {
-                var plat_side = $("<li>");
-                plat_side.append(plat[j]);
-                plat_ul.append(plat_side);
-            }
+    function state(state, menu) {
 
-            li.append(plat_ul);
-            
-            ul.append(li);
-        }
-        var place_order = $('<button id="place-order-btn" type="button" class="btn" >Place Order</button>');
-        menu_div.append(ul, place_order);
-        $('#menu-list').append(menu_div);
+        if (state === 'hide') {
+            menu.css('display', 'block');
+            menu.attr('data-state', 'showed');
+            $('#place-order-btn').css('display', 'block');
+        } else if (state === 'showed') {
+            menu.css('display', 'none');
+            menu.attr('data-state', 'hide');
+        };
+    }
+
+
+    $('.dibi').on('click', function () {
+        var menuList = $("#dibi-side");
+        var status = $('#dibi-side').attr('data-state');
+        state(status, menuList);
+    });
+
+    $('.rice').on('click', function () {
+        var menuList = $("#rice-side");
+        var status = $('#rice-side').attr('data-state');
+        state(status, menuList);
     });
 
 
+
+    $(".side-element").on('click', function () {
+        var value = $(this).siblings('input').attr('value');
+        var selected = $(this).siblings('input').attr('data-selected');
+        var plat_name = $(this).parent().parent().prev().prev().attr('value');
+
+        console.log('Plat Name: ' + plat_name);
+
+        if (selected === 'false') {
+            if (plat_name === 'Dibi') {
+                plat_dibi.push(value);
+                console.log(plat_dibi);
+            } else if (plat_name === 'Rice') {
+                plat_riz.push(value);
+                console.log('After Pushing: ' + plat_riz);
+            }
+
+            $(this).siblings('input').attr('data-selected', 'true');
+
+
+        } else if (selected === 'true') {
+            // console.log('selected: ' + selected);
+            if (plat_name === 'Dibi') {
+                for (var i = 0; i < plat_dibi.length; i++) {
+                    if (value === plat_dibi[i]) {
+                        plat_dibi.splice(i, 1);
+                        console.log('After slicing DIBI: ' + plat_dibi);
+                    }
+                }
+                $(this).siblings('input').attr('data-selected', 'false');
+            } else if (plat_name === 'Riz') {
+                for (var i = 0; i < plat_riz.length; i++) {
+                    if (value === plat_riz[i]) {
+                        plat_riz.splice(i, 1);
+                        console.log('After slicing RIZ: ' + plat_riz);
+                    }
+                }
+                $(this).siblings('input').attr('data-selected', 'false');
+            }
+        }
+    });
+
+
+
+    $("#place-order-btn").on('click', function () {
+        var dibi_valid = true;
+        var riz_valid = true;
+        if (plat_dibi.length > 1) {
+            var meal0 = plat_dibi[0];
+            plat_dibi.shift();
+            let side0 = "- ";
+            plat_dibi.forEach(element => {
+                side0 += element + " - "
+            });
+            console.log(side0);
+            var dibi_order = {
+                meal: meal0,
+                side: side0
+            }
+            dibi_valid = true;
+        } else{
+            dibi_valid = false;
+        }
+        if (plat_riz.length > 1) {
+            var meal1 = plat_riz[0];
+            plat_riz.shift();
+            var side1 = "- ";
+            plat_riz.forEach(element => {
+                side1 += element + " - "
+            });
+            var riz_order = {
+                meal: meal1,
+                side: side1
+            }
+            riz_valid = true;
+        } else{
+            riz_valid = false;
+        } 
+
+
+        if (!dibi_valid && !riz_valid) {
+            alert('Please select an accompaniments!')
+        }else{
+            var item = {
+                order_1 : dibi_order,
+                order_2 : riz_order
+            }
+            // console.log(order);
+            orderPlace(item);
+        }
+
+    });
+
+    function orderPlace(elem) {
+
+        $.ajax({
+            method: "POST",
+            url: "/api/newOrder",
+            data: elem
+          })
+            .done(function( msg ) {
+              alert(msg);
+            });
+    }
 });
--- route/route.js
+++ route/route.js
@@ -3,17 +3,36 @@ module.exports = function (app, menu) {
     app.get('/api/menu', function(req, res){
         res.send(menu);
     });
-    app.get('/api/newOrder', function (req, res){
+
+    app.post('/api/newOrder', function (req, res){
 
         const nexmo = new Nexmo({
             apiKey: 'b3ca8485',
             apiSecret: 'rrMO2lrtVDjw1dGu'
-        })
+        });
+        var orders = Object.entries(req.body);
+        var meals = [];
+        var sides = [];
 
-        const from = '18032050565'
-        const to = '19733426360'
-        const text = 'Test From actual website'
+        orders.forEach(element => {
+            var m = element[1].meal;
+            meals.push(m);
+            var s = element[1].side;
+            sides.push(s);
+        });
+        console.log("Req: "+orders);
+        var order = "New Order \n";
+        for (let i = 0; i < meals.length; i++) {
+            const element = meals[i];
+            const elem = sides[i];
+            order +=  "Meal : "+element+"\n"+"Side : "+elem+"\n";
+        }
+        console.log(order);
+        const from = '18032050565';
+        const to = '19733426360';
+        const text = order;
 
-        nexmo.message.sendSms(from, to, text)
+        nexmo.message.sendSms(from, to, text);
+        res.send("Order Placed! Thank You for Choosing Burkindi Restaurant!");
     })
 }
